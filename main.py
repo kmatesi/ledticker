0b89e0864ae3f2ba1e8a14bbdaa2f439976da88e
kmatesi <kevin.matesi@gmail.com>
2015-01-16 14:51:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 from baseticker.client import SignClient
 import os
 import time
-import utils
 import get_scores
 import display_scores
 #-------------------------------------------------------------------------------
@@ -17,10 +16,21 @@ def start_msg():
     start = ['Starting', 'Sports Ticker']
     SignClient(glyphs_path, led_sign_path).send_text_to_sign(start);
 #---------------------------------------------------------------------------
+def send_msg(entry):
+    # New client to write to led sign
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    led_sign_path = '/'.join([pwd, 'led_sign']) 
+    glyphs_path = '/'.join([led_sign_path, 'glyphs'])
+
+    sign_client = SignClient(glyphs_path, led_sign_path)
+    sign_client.send_text_to_sign(entry)
+    time.sleep(6)
+
 if __name__ == "__main__":
     start_msg()
 
-    while True:
-        get_scores.main()
-        display_scores.main()
+    #while True:
+    #    scores = get_scores.main()
+    #    for line in scores:
+    #        send_msg(line)
 #===============================================================================
